5abda594a3a888296e189a5a02860ae9521d9c3e
s19366 <s19366@pjwstk.edu.pl>
2025-06-22 16:28:00 +0200

    Dodano metodę

--- AplikacjaKonsolowa/Program.cs
+++ AplikacjaKonsolowa/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Modyfikacja 3");
+using System;
+
+namespace AplikacjaKonsolowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dane = { 1, 2, 3, 4, 5 };
+            double srednia = ObliczSrednia(dane);
+            Console.WriteLine("Średnia: " + srednia);
+        }
+
+        public static double ObliczSrednia(int[] liczby)
+        {
+            if (liczby == null || liczby.Length == 0)
+                return 0;
+
+            double suma = 0;
+            foreach (int liczba in liczby)
+            {
+                suma += liczba;
+            }
+
+            return suma / liczby.Length;
+        }
+    }
+}
